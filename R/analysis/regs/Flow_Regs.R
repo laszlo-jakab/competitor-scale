50e1895825b3c74c4c04ec77596e826b3eaf3e17
laszlo-jakab <laszlo.jakab@chicagobooth.edu>
2018-03-16 17:13:32 -0500

    small date changes

--- R/analysis/regs/Flow_Regs.R
+++ R/analysis/regs/Flow_Regs.R
@@ -13,9 +13,24 @@ source("R/functions/RegDisplay_Fn.R")
 combined <- readRDS("data/combined/combined.Rds")
 scandal  <- readRDS("data/scandal/")
 
-# Flows and Competitor Scale ---------------------------------------
+
+# Data Prep ---------------------------------------------------
+
+# net log flows
+combined[, flow.log.raw := log(tna / (tna.lagged * (1 + r.net / (12 * 100))))]
+combined[, flow.log := psych::winsor(flow.log.raw, trim = .01)]
+
+combined[, diff := round((date - shift(date))*12), by = wficn]
+combined[diff == 1, flow.next := shift(flow, type = "lead"), by = wficn]
+
+merge(cpi, copy(cpi)[, date := date + 1/12], by = "date", all.x = TRUE)
+
+
+# Flows and Competitor Scale ---------------------------------------------------
 
 
 
+
+hist(combined[, flow])
 
 
