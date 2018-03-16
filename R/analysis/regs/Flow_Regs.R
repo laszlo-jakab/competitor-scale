6d1ceac5a6004accde76f088f0bd8721d4a11eb4
laszlo-jakab <laszlo.jakab@chicagobooth.edu>
2018-03-16 13:03:28 -0500

    Add scandal data

---
+++ R/analysis/regs/Flow_Regs.R
@@ -0,0 +1,21 @@
+# Laszlo Jakab
+# Mar, 2018
+
+# Setup ------------------------------------------------------------------------
+
+# packages
+library(lfe)
+
+# source functions for displaying regressions
+source("R/functions/RegDisplay_Fn.R")
+
+# load data
+combined <- readRDS("data/combined/combined.Rds")
+scandal  <- readRDS("data/scandal/")
+
+# Flows and Competitor Scale ---------------------------------------
+
+
+
+
+
