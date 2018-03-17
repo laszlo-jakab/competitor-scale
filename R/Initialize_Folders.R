44b8a7d4af33ac05d8e69c4c5580b5f567134013
laszlo-jakab <laszlo.jakab@chicagobooth.edu>
2018-03-17 00:24:30 -0500

    add script to create data folder structure

---
+++ R/Initialize_Folders.R
@@ -0,0 +1,11 @@
+# Laszlo Jakab
+# Mar 2018
+
+# create folder structure to contain data
+dir.create("data/benchmarked_returns")
+dir.create("data/clean")
+dir.create("data/combined")
+dir.create("data/competitor_size")
+dir.create("data/funds_included")
+dir.create("data/portfolio")
+dir.create("raw")
