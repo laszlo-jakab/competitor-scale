8a9a89bba55860779b92a53e2373845944659efd
laszlo-jakab <laszlo.jakab@chicagobooth.edu>
2018-03-28 15:46:03 -0500

    table titles

--- R/analysis/regs/Behavior_Regs.r
+++ R/analysis/regs/Behavior_Regs.r
@@ -72,7 +72,7 @@ rt.fd <- RegTable(r.fd, fe.list = fe.list, coef.lab.dt = coef.lab.dt)
 # label table
 tab.fd <- list(
   results = rt.fd,
-  title = "Capital Allocation and Competitor Size",
+  title = "Capital Allocation and Competitor Scale",
   caption = "Observations are at the fund-quarter level, from 1980-2016. $\\Delta$ denotes first differences. Dependent variables are noted in the column headers. $AS$ is active share relative to self-declared benchmarks from \\citet{petajisto13}, covering years 1980-2009. $T$ is turnover; $L$, $S$, $D$, $C$, and $B$ are respectively portfolio liquidity, stock liquidity, diversification, coverage, and balance, as defined in \\citet{pst17L}. $\\Delta CS_{i,t}=\\ln\\left(\\sum_{j\\neq i} \\psi_{i,j,t-1} FundSize_{j,t} \\right) - \\ln\\left(\\sum_{j\\neq i} \\psi_{i,j,t-1} FundSize_{j,t-1}\\right)$ is the change in log competitor size, holding previous quarter end similarity weights fixed. Standard errors are double clustered by fund and portfolio group $\\times$ quarter, and reported in parentheses. Asterisks denote statistical significance: *** $p<$0.01, ** $p<$0.05, * $p<$0.1.")
 
 
@@ -87,7 +87,7 @@ rt.fd.08 <- RegTable(r.fd.08, fe.list = fe.list, coef.lab.dt = coef.lab.dt)
 # label table
 tab.fd.08 <- list(
   results = rt.fd.08,
-  title = "Capital Allocation and Competitor Size --- Pre-2008 Data",
+  title = "Capital Allocation and Competitor Scale --- Pre-2008 Data",
   caption = "Observations are at the fund-quarter level, from 1980-2007. $\\Delta$ denotes first differences. Dependent variables are noted in the column headers. $AS$ is active share relative to self-declared benchmarks from \\citet{petajisto13}. $T$ is turnover; $L$, $S$, $D$, $C$, and $B$ are respectively portfolio liquidity, stock liquidity, diversification, coverage, and balance, as defined in \\citet{pst17L}. $\\Delta CS_{i,t}=\\ln\\left(\\sum_{j\\neq i} \\psi_{i,j,t-1} FundSize_{j,t} \\right) - \\ln\\left(\\sum_{j\\neq i} \\psi_{i,j,t-1} FundSize_{j,t-1}\\right)$ is the change in log competitor size, holding previous quarter end similarity weights fixed. Standard errors are double clustered by fund and portfolio group $\\times$ quarter, and reported in parentheses. Asterisks denote statistical significance: *** $p<$0.01, ** $p<$0.05, * $p<$0.1.")
 
 
@@ -110,7 +110,7 @@ rt.fd.bm <- RegTable(r.fd.bm, fe.list = fe.list.bm, coef.lab.dt = coef.lab.dt)
 # label table
 tab.fd.bm <- list(
   results = rt.fd.bm,
-  title = "Capital Allocation and Competitor Size --- Benchmark $\\times$ Quarter FE",
+  title = "Capital Allocation and Competitor Scale --- Benchmark $\\times$ Quarter FE",
   caption = "Observations are at the fund-quarter level, from 1980-2007. $\\Delta$ denotes first differences. Dependent variables are noted in the column headers. $AS$ is active share relative to self-declared benchmarks from \\citet{petajisto13}. $T$ is turnover; $L$, $S$, $D$, $C$, and $B$ are respectively portfolio liquidity, stock liquidity, diversification, coverage, and balance, as defined in \\citet{pst17L}. Benchmarks are the indexes which yield the lowest active share, taken from \\citet{petajisto13}. I use the most recently available benchmark when one is missing. Standard errors are double clustered by fund and benchmark $\\times$ quarter, and reported in parentheses. Asterisks denote statistical significance: *** $p<$0.01, ** $p<$0.05, * $p<$0.1.")
 
 
@@ -126,7 +126,7 @@ rt.fd.bm.08 <- RegTable(r.fd.bm.08,
 # label table
 tab.fd.bm.08 <- list(
   results = rt.fd.bm.08,
-  title = "Capital Allocation and Competitor Size --- Benchmark $\\times$ Quarter FE, Pre-2008 Data",
+  title = "Capital Allocation and Competitor Scale --- Benchmark $\\times$ Quarter FE, Pre-2008 Data",
   caption = "Observations are at the fund-quarter level, from 1980-2007. $\\Delta$ denotes first differences. Dependent variables are noted in the column headers. $AS$ is active share relative to self-declared benchmarks from \\citet{petajisto13}. $T$ is turnover; $L$, $S$, $D$, $C$, and $B$ are respectively portfolio liquidity, stock liquidity, diversification, coverage, and balance, as defined in \\citet{pst17L}. $\\Delta CS_{i,t}=\\ln\\left(\\sum_{j\\neq i} \\psi_{i,j,t-1} FundSize_{j,t} \\right) - \\ln\\left(\\sum_{j\\neq i} \\psi_{i,j,t-1} FundSize_{j,t-1}\\right)$ is the change in log competitor size, holding previous quarter end similarity weights fixed. Benchmarks are the indexes which yield the lowest active share, taken from \\citet{petajisto13}. I use the most recently available benchmark when one is missing. Standard errors are double clustered by fund and benchmark $\\times$ quarter, and reported in parentheses. Asterisks denote statistical significance: \\*** $p<$0.01, ** $p<$0.05, * $p<$0.1.")
 
 
